e709ab1fbdb2defa2a7c46e334b963e18ffe5519
Kay Hudson <kay.hudson@galvanize.com>
2020-05-05 18:26:34 -0700

    Add Docker commands

--- src/main/java/com/galvanize/Stack.java
+++ src/main/java/com/galvanize/Stack.java
@@ -1,1 +1,34 @@
 package com.galvanize;
+
+import java.util.ArrayList;
+
+public class Stack {
+    public boolean empty;
+    private ArrayList items;
+
+    public Stack() {
+        this.empty = true;
+        this.items = new ArrayList();
+    }
+
+    public int count() {
+        return this.items.size();
+    }
+
+    public void push(Object item) {
+        this.items.add(item);
+        this.empty = false;
+    }
+
+    public Object pop() {
+        int lastIndex = this.count() -1;
+        Object lastItem = this.items.get(lastIndex);
+        this.items.remove(lastIndex);
+        return lastItem;
+    }
+
+    public Object peek() {
+        int lastIndex = this.count() -1;
+        return this.items.get(lastIndex);
+    }
+}
