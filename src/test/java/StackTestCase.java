27b89d026faa90cda0ff2cee56f2e967d7f73e4d
Kay Hudson <kay.hudson@galvanize.com>
2020-05-05 17:21:38 -0700

    Add tests for pop and peek.

--- src/test/java/StackTestCase.java
+++ src/test/java/StackTestCase.java
@@ -6,11 +6,31 @@ import com.galvanize.Stack;
 public class StackTestCase {
 
     @Test
-    public void test_stack_push() {
+    public void testStackPush() {
         Stack newStack = new Stack();
+        assertEquals(0, newStack.count());
         newStack.push(100);
+        assertEquals(1, newStack.count());
         assertFalse(newStack.empty);
     }
 
     @Test
+    public void testStackPopRemovesAndReturnsLastItemIn() {
+        Stack newStack = new Stack();
+        newStack.push(1);
+        newStack.push(2);
+        newStack.push(3);
+        assertEquals(3, newStack.count());
+        Object item = newStack.pop();
+        assertEquals(2, newStack.count());
+        assertEquals(3, item);
+    }
+
+    @Test
+    public void testStackPeek() {
+        Stack newStack = new Stack();
+        newStack.push(1);
+        newStack.push(2);
+        assertEquals(2, newStack.peek());
+    }
 }
